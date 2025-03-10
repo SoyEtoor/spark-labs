2d0db11072b09ee8354845ef7a09438e2389ea9c
Héctor Miguel <127361651+SoyEtoor@users.noreply.github.com>
2025-03-10 15:57:46 -0600

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession
+
+# Create a spark session
+spark = SparkSession.builder.appName("HelloWorld").getOrCreate()
+
+# Use sql() to write a raw SQL query
+df = spark.sql("SELECT 'Hello World' as hello")
+
+# Print the dataframe
+df.show()
+df.write.mode("overwrite").json("results")
+
+    - run: spark-submit --version
+    - run: spark-submit --master local hello.py
+    - run: ls -la  
